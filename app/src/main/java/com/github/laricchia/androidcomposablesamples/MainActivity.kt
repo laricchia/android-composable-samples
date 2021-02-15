c6307741fd3aaf72ceabdb5aee64a752a9cfb6c8
robertoAsus <robertox92@gmail.com>
2021-02-15 21:53:09 +0100

    [app] android sketch

---
+++ app/src/main/java/com/github/laricchia/androidcomposablesamples/MainActivity.kt
@@ -0,0 +1,38 @@
+ package com.github.laricchia.androidcomposablesamples
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.laricchia.androidcomposablesamples.ui.theme.AndroidComposableSamplesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidComposableSamplesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidComposableSamplesTheme {
+        Greeting("Android")
+    }
+}
